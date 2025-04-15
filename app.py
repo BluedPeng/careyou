664cb6ed1ad1fce3bb1e33e78cb7641450f9e428
BluedPeng <AIXNS@careyou.com>
2025-04-15 23:09:03 +0800

    merge cmd into one app.py. Deleting debugging.

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM  # isort: skip
 from openxlab.model import download
 
 logger = logging.get_logger(__name__)
+os.system('apt install git-lfs')
 os.system("python download_model.py AIXNS/careyou_7b_16bit_v3_2")
 os.system('pip install -r requirements.txt')
 
