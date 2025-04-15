cc48bcf955de7d92cc41615f23f762536106c64b
BluedPeng <AIXNS@careyou.com>
2025-04-15 16:25:55 +0800

    debugging

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 os.system("python download_model.py AIXNS/careyou_7b_16bit_v3_2")
 os.system('pip install -r requirements.txt')
-os.system('streamlit run /home/xlab-app-center/web_careyou.py')   
+os.system('streamlit run /home/xlab-app-center/web_test.py')   
