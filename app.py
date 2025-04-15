694123728f7da9bd7931bf461e5991bb720f1c23
BluedPeng <AIXNS@careyou.com>
2025-04-15 13:56:33 +0800

    find the port error and fix it. Deleting debugging

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
 import os
 os.system("python download_model.py AIXNS/careyou_7b_16bit_v3_2")
-os.system('cd model')
-os.system('ls')
-os.system('cd ../')
-os.system('streamlit run web_careyou.py --server.address=0.0.0.0 --server.port 7860')   
+os.system('streamlit run web_careyou.py --server.address=0.0.0.0 --server.port 1546')   
