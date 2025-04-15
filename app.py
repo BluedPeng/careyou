66e214bf7b0a1325453c7dae1084585671e602b9
BluedPeng <AIXNS@careyou.com>
2025-04-15 15:05:34 +0800

    debugging

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import os
 os.system("python download_model.py AIXNS/careyou_7b_16bit_v3_2")
-os.system('streamlit run web_careyou.py --server.address=0.0.0.0 --server.port 1546')   
+os.system('cd model & ls')
+os.system('pwd')
+os.system('streamlit run web_careyou.py --server.port 8053')   
