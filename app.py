471ad27b5bedc796e0f83ce1f77cf293f620e5f0
BluedPeng <AIXNS@careyou.com>
2025-04-15 16:10:03 +0800

    debugging

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
 os.system("python download_model.py AIXNS/careyou_7b_16bit_v3_2")
-os.system('cd model && pwd && ls')
-os.system('pwd')
+os.system('pip install -r requirements.txt')
 os.system('streamlit run /home/xlab-app-center/web_careyou.py')   
